097a3fd0e38e369880d65f0eca4fd83afce4481a
ValexEvans <57923977+ValexEvans@users.noreply.github.com>
2024-02-05 17:43:49 -0500

    landing page update

--- js/landing.js
+++ js/landing.js
@@ -1,14 +1,14 @@
-let signup = document.querySelector(".color");
-let login = document.querySelector(".contacts");
+let contacts = document.querySelector(".contacts");
+let viewer = document.querySelector(".color");
 let slider = document.querySelector(".slider");
 let formSection = document.querySelector(".form-section");
 
-signup.addEventListener("click", () => {
+contacts.addEventListener("click", () => {
 	slider.classList.add("moveslider");
 	formSection.classList.add("form-section-move");
 });
 
-login.addEventListener("click", () => {
+viewer.addEventListener("click", () => {
 	slider.classList.remove("moveslider");
 	formSection.classList.remove("form-section-move");
 });
