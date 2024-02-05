f9e70afc9c296ba6cea797148cbbdf2f844c3042
ValexEvans <57923977+ValexEvans@users.noreply.github.com>
2024-02-05 11:10:57 -0500

    code update

--- js/code.js
+++ js/code.js
@@ -1,310 +1,262 @@
-const urlBase = '138.197.67.189';
-const extension = 'php';
+const urlBase = "127.0.0.1:5500";
+const extension = "php";
 
 let userId = 0;
 let firstName = "";
 let lastName = "";
 
-function doLogin()
-{
-	userId = 0;
-	firstName = "";
-	lastName = "";
-	
-	let login = document.getElementById("loginName").value;
-	let password = document.getElementById("loginPassword").value;
-	console.log("user/pass= " + login);
-	console.log(password);
-//	var hash = md5( password );
-	
-	document.getElementById("loginResult").innerHTML = "";
+function doLogin() {
+  userId = 0;
+  firstName = "";
+  lastName = "";
 
-	let tmp = {login:login,password:password};
-//	var tmp = {login:login,password:hash};
-	let jsonPayload = JSON.stringify( tmp );
-	console.log("jsonPayload= " + jsonPayload);
-	
-	let url = 'http://' + urlBase + '/LAMPAPI/Login.' + extension;
-	//let url = '/var/www/html/LAMPAPI/Login.' + extension;
+  let login = document.getElementById("loginName").value;
+  let password = document.getElementById("loginPassword").value;
+  console.log("user/pass= " + login);
+  console.log(password);
+  //	var hash = md5( password );
 
-	let xhr = new XMLHttpRequest();
-	xhr.open("POST", url, true);
-	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-	try
-	{
-		xhr.onreadystatechange = function() 
-		{
-			if (this.readyState == 4 && this.status == 200) 
-			{
-				let jsonObject = JSON.parse( xhr.responseText );
-				userId = jsonObject.id;
-		
-				if( userId < 1 )
-				{		
-					document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
-					return;
-				}
-		
-				firstName = jsonObject.firstName;
-				lastName = jsonObject.lastName;
+  document.getElementById("loginResult").innerHTML = "";
 
-				saveCookie();
-	
-				window.location.href = "landing.html";
-			}
-		};
-		xhr.send(jsonPayload);
-	}
-	catch(err)
-	{
-		document.getElementById("loginResult").innerHTML = err.message;
-	}
+  let tmp = { login: login, password: password };
+  //	var tmp = {login:login,password:hash};
+  let jsonPayload = JSON.stringify(tmp);
+  console.log("jsonPayload= " + jsonPayload);
 
+  let url = "http://" + urlBase + "/LAMPAPI/Login." + extension;
+  //let url = '/var/www/html/LAMPAPI/Login.' + extension;
+
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", url, true);
+  xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+  try {
+    xhr.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        let jsonObject = JSON.parse(xhr.responseText);
+        userId = jsonObject.id;
+
+        if (userId < 1) {
+          document.getElementById("loginResult").innerHTML =
+            "User/Password combination incorrect";
+          return;
+        }
+
+        firstName = jsonObject.firstName;
+        lastName = jsonObject.lastName;
+
+        saveCookie();
+
+        window.location.href = "landing.html";
+      }
+    };
+    xhr.send(jsonPayload);
+  } catch (err) {
+    document.getElementById("loginResult").innerHTML = err.message;
+  }
 }
 
-function doLogout()
-{
-	userId = 0;
-	firstName = "";
-	lastName = "";
-	document.cookie = "firstName= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
-	window.location.href = "index.html";
+function doLogout() {
+  userId = 0;
+  firstName = "";
+  lastName = "";
+  document.cookie = "firstName= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
+  window.location.href = "index.html";
 }
 
-function addContact()
-{
-	let firstName = document.getElementById("contactFirst").value;
-	let lastName = document.getElementById("contactLast").value;
-	let phone = document.getElementById("contactPhone").value;
-	let email = document.getElementById("contactEmail").value;
-	document.getElementById("contactAddResult").innerHTML = "";
+function doRegister() {
+  // reset required red borders
+  document.getElementById("registerFirstName").className = "ele";
+  document.getElementById("registerLastName").className = "ele";
+  document.getElementById("registerLogin").className = "ele";
+  document.getElementById("registerPassword").className = "ele";
 
-	let tmp = {firstName:firstName,lastName:lastName,phone:phone,email:email,userId:userId};
-	let jsonPayload = JSON.stringify( tmp );
+  // collect values from form
+  let firstName = document.getElementById("registerFirstName").value;
+  let lastName = document.getElementById("registerLastName").value;
+  let login = document.getElementById("registerLogin").value;
+  let password = document.getElementById("registerPassword").value;
 
-	let url = urlBase + '/AddColor.' + extension;
-	
-	let xhr = new XMLHttpRequest();
-	xhr.open("POST", url, true);
-	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-	try
-	{
-		xhr.onreadystatechange = function() 
-		{
-			if (this.readyState == 4 && this.status == 200) 
-			{
-				document.getElementById("colorAddResult").innerHTML = "Color has been added";
-			}
-		};
-		xhr.send(jsonPayload);
-	}
-	catch(err)
-	{
-		document.getElementById("colorAddResult").innerHTML = err.message;
-	}
-	
+  if (firstName === "") {
+    document.getElementById("registerFirstName").className = "ele required";
+    document.getElementById("registerResult").innerHTML =
+      "Please fill out all fields";
+    return;
+  }
+  if (lastName === "") {
+    document.getElementById("registerLastName").className = "ele required";
+    document.getElementById("registerResult").innerHTML =
+      "Please fill out all fields";
+    return;
+  }
+  if (login === "") {
+    document.getElementById("registerLogin").className = "ele required";
+    document.getElementById("registerResult").innerHTML =
+      "Please fill out all fields";
+    return;
+  }
+  if (password === "") {
+    document.getElementById("registerPassword").className = "ele required";
+    document.getElementById("registerResult").innerHTML =
+      "Please fill out all fields";
+    return;
+  }
+
+  // create json payload
+  let tmp = {
+    login: login,
+    password: password,
+    firstName: firstName,
+    lastName: lastName,
+  };
+
+  // resets fields
+  userId = 0;
+  firstName = "";
+  lastName = "";
+  document.getElementById("registerResult").innerHTML = "";
+
+  let jsonPayload = JSON.stringify(tmp);
+  console.log("jsonPayload= " + jsonPayload);
+
+  let url = "http://" + urlBase + "/LAMPAPI/Register." + extension;
+
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", url, true);
+  xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+  try {
+    xhr.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        let jsonObject = JSON.parse(xhr.responseText);
+        userId = jsonObject.id;
+
+        if (userId < 1) {
+          let error = jsonObject.error;
+          document.getElementById("loginResult").innerHTML = err;
+          return;
+        }
+
+        window.location.reload();
+      }
+    };
+    xhr.send(jsonPayload);
+  } catch (err) {
+    document.getElementById("loginResult").innerHTML = err.message;
+  }
 }
 
-function doRegister()
-{
-	// reset required red borders
-	document.getElementById("registerFirstName").className = "ele";
-	document.getElementById("registerLastName").className = "ele";
-	document.getElementById("registerLogin").className = "ele";
-	document.getElementById("registerPassword").className = "ele";
-	
-	// collect values from form
-	let firstName = document.getElementById("registerFirstName").value;
-	let lastName = document.getElementById("registerLastName").value;
-	let login = document.getElementById("registerLogin").value;
-	let password = document.getElementById("registerPassword").value;
+function addContact() {
+  document.getElementById("contactFirst").className = "ele";
+  document.getElementById("contactLast").className = "ele";
+  document.getElementById("contactPhone").className = "ele";
+  document.getElementById("contactEmail").className = "ele";
 
-	if (firstName === "")
-	{
-		document.getElementById("registerFirstName").className = "ele required";
-		document.getElementById("registerResult").innerHTML = "Please fill out all fields";
-		return;
-	}
-	if (lastName === "")
-	{
-		document.getElementById("registerLastName").className = "ele required";
-		document.getElementById("registerResult").innerHTML = "Please fill out all fields";
-		return;
-	}
-	if (login === "")
-	{
-		document.getElementById("registerLogin").className = "ele required";
-		document.getElementById("registerResult").innerHTML = "Please fill out all fields";
-		return;
-	}
-	if (password === "")
-	{
-		document.getElementById("registerPassword").className = "ele required";
-		document.getElementById("registerResult").innerHTML = "Please fill out all fields";
-		return;
-	}
-	
+  // collect values from form
+  let firstName = document.getElementById("contactFirst").value;
+  let lastName = document.getElementById("contactLast").value;
+  let phone = document.getElementById("contactPhone").value;
+  let email = document.getElementById("contactEmail").value;
 
-	// resets fields
-	userId = 0;
-	firstName = "";
-	lastName = "";
-	document.getElementById("registerResult").innerHTML = "";
+  if (firstName === "") {
+    document.getElementById("contactFirst").className = "ele required";
+    document.getElementById("contactAddResult").innerHTML =
+      "Please fill out all fields";
+    return;
+  }
+  if (lastName === "") {
+    document.getElementById("contactLast").className = "ele required";
+    document.getElementById("contactAddResult").innerHTML =
+      "Please fill out all fields";
+    return;
+  }
+  if (phone === "") {
+    document.getElementById("contactPhone").className = "ele required";
+    document.getElementById("contactAddResult").innerHTML =
+      "Please fill out all fields";
+    return;
+  }
+  if (email === "") {
+    document.getElementById("contactEmail").className = "ele required";
+    document.getElementById("contactAddResult").innerHTML =
+      "Please fill out all fields";
+    return;
+  }
 
-	// create json payload
-	let tmp = {login:login,password:password,firstName:firstName,lastName:lastName};
-	let jsonPayload = JSON.stringify( tmp );
-	console.log("jsonPayload= " + jsonPayload);
-	
-	let url = 'http://' + urlBase + '/LAMPAPI/Register.' + extension;
+  let tmp = {
+    firstName: firstName,
+    lastName: lastName,
+    phone: phone,
+    email: email,
+    userId: userId,
+  };
+  // resets fields
+  userId = 0;
+  firstName = "";
+  lastName = "";
+  phone = "";
+  email = "";
+  document.getElementById("contactAddResult").innerHTML = "";
 
-	let xhr = new XMLHttpRequest();
-	xhr.open("POST", url, true);
-	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-	try
-	{
-		xhr.onreadystatechange = function() 
-		{
-			if (this.readyState == 4 && this.status == 200) 
-			{
-				let jsonObject = JSON.parse( xhr.responseText );
-				userId = jsonObject.id;
-		
-				if( userId < 1 )
-				{
-					let error = jsonObject.error;
-					document.getElementById("loginResult").innerHTML = err;
-					return;
-				}
-	
-				window.location.reload();
-			}
-		};
-		xhr.send(jsonPayload);
-	}
-	catch(err)
-	{
-		document.getElementById("loginResult").innerHTML = err.message;
-	}
+  let jsonPayload = JSON.stringify(tmp);
+  console.log("jsonPayload= " + jsonPayload);
+
+  let url = "http://" + urlBase + "/LAMPAPI/AddContact." + extension;
+
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", url, true);
+  xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+  try {
+    xhr.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        let jsonObject = JSON.parse(xhr.responseText);
+        userId = jsonObject.id;
+
+        if (userId < 1) {
+          let error = jsonObject.error;
+          document.getElementById("contactAddResult").innerHTML = err;
+          return;
+        }
+        window.location.reload();
+      }
+    };
+    xhr.send(jsonPayload);
+  } catch (err) {
+    document.getElementById("contactAddResult").innerHTML = err.message;
+  }
 }
 
-function saveCookie()
-{
-	let minutes = 20;
-	let date = new Date();
-	date.setTime(date.getTime()+(minutes*60*1000));	
-	document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + ";expires=" + date.toGMTString();
-}
+function searchContact() {
+  let srch = document.getElementById("searchText").value;
+  document.getElementById("colorSearchResult").innerHTML = "";
 
-function readCookie()
-{
-	userId = -1;
-	let data = document.cookie;
-	let splits = data.split(",");
-	for(var i = 0; i < splits.length; i++) 
-	{
-		let thisOne = splits[i].trim();
-		let tokens = thisOne.split("=");
-		if( tokens[0] == "firstName" )
-		{
-			firstName = tokens[1];
-		}
-		else if( tokens[0] == "lastName" )
-		{
-			lastName = tokens[1];
-		}
-		else if( tokens[0] == "userId" )
-		{
-			userId = parseInt( tokens[1].trim() );
-		}
-	}
-	
-	if( userId < 0 )
-	{
-		window.location.href = "index.html";
-	}
-	else
-	{
-		document.getElementById("userName").innerHTML = "Logged in as " + firstName + " " + lastName;
-	}
-}
+  let colorList = "";
 
+  let tmp = { search: srch, userId: userId };
+  let jsonPayload = JSON.stringify(tmp);
 
-function addContact()
-{
-	let firstName = document.getElementById("contactFirst").value;
-	let lastName = document.getElementById("contactLast").value;
-	let phone = document.getElementById("contactPhone").value;
-	let email = document.getElementById("contactEmail").value;
-	document.getElementById("contactAddResult").innerHTML = "";
+  let url = urlBase + "/SearchColors." + extension;
 
-	let tmp = {firstName:firstName,lastName:lastName,phone:phone,email:email,userId:userId};
-	let jsonPayload = JSON.stringify( tmp );
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", url, true);
+  xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+  try {
+    xhr.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("colorSearchResult").innerHTML =
+          "Color(s) has been retrieved";
+        let jsonObject = JSON.parse(xhr.responseText);
 
-	let url = urlBase + '/AddColor.' + extension;
-	
-	let xhr = new XMLHttpRequest();
-	xhr.open("POST", url, true);
-	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-	try
-	{
-		xhr.onreadystatechange = function() 
-		{
-			if (this.readyState == 4 && this.status == 200) 
-			{
-				document.getElementById("colorAddResult").innerHTML = "Color has been added";
-			}
-		};
-		xhr.send(jsonPayload);
-	}
-	catch(err)
-	{
-		document.getElementById("colorAddResult").innerHTML = err.message;
-	}
-	
-}
+        for (let i = 0; i < jsonObject.results.length; i++) {
+          colorList += jsonObject.results[i];
+          if (i < jsonObject.results.length - 1) {
+            colorList += "<br />\r\n";
+          }
+        }
 
-function searchColor()
-{
-	let srch = document.getElementById("searchText").value;
-	document.getElementById("colorSearchResult").innerHTML = "";
-	
-	let colorList = "";
-
-	let tmp = {search:srch,userId:userId};
-	let jsonPayload = JSON.stringify( tmp );
-
-	let url = urlBase + '/SearchColors.' + extension;
-	
-	let xhr = new XMLHttpRequest();
-	xhr.open("POST", url, true);
-	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-	try
-	{
-		xhr.onreadystatechange = function() 
-		{
-			if (this.readyState == 4 && this.status == 200) 
-			{
-				document.getElementById("colorSearchResult").innerHTML = "Color(s) has been retrieved";
-				let jsonObject = JSON.parse( xhr.responseText );
-				
-				for( let i=0; i<jsonObject.results.length; i++ )
-				{
-					colorList += jsonObject.results[i];
-					if( i < jsonObject.results.length - 1 )
-					{
-						colorList += "<br />\r\n";
-					}
-				}
-				
-				document.getElementsByTagName("p")[0].innerHTML = colorList;
-			}
-		};
-		xhr.send(jsonPayload);
-	}
-	catch(err)
-	{
-		document.getElementById("colorSearchResult").innerHTML = err.message;
-	}
-	
+        document.getElementsByTagName("p")[0].innerHTML = colorList;
+      }
+    };
+    xhr.send(jsonPayload);
+  } catch (err) {
+    document.getElementById("colorSearchResult").innerHTML = err.message;
+  }
 }
